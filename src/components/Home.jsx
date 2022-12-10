28c588b24cc5de788e321313920cabeb0ba2f227
Aman9458 <webdevaman2@gmail.com>
2022-12-10 22:56:14 +0530

    changes in route

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,61 +1,78 @@
-import React from 'react'
-import { motion } from 'framer-motion'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { motion } from "framer-motion";
+import { Link } from "react-router-dom";
 function Home() {
   return (
     <>
-    <motion.section 
-    initial={{ opacity: 0, scale: 0.7 }}
-    animate={{ opacity: 1, scale: 1 }}
-    transition={{
-      duration: 0.8,
-      delay: 0.5,
-      ease: [0, 0.71, 0.2, 1.01]
-    }}
-    className='common-seciton lg:w-[80%]'>
+      <motion.section
+        initial={{ opacity: 0, scale: 0.7 }}
+        animate={{ opacity: 1, scale: 1 }}
+        transition={{
+          duration: 0.8,
+          delay: 0.5,
+          ease: [0, 0.71, 0.2, 1.01],
+        }}
+        className="common-seciton lg:w-[80%]"
+      >
         <div className="container flex flex-col-reverse md:flex-row">
-            <div className="left flex-1 text-xs sm:text-sm md:text-base xl:flex-[1.3]">
-                <div className="leading-8"><h1 className='text-xl md:text-2xl lg:text-4xl xl:text-6xl'>Hey, I am a</h1></div>
-                <div className="">
-                    <h1 className='text-xl md:text-2xl lg:text-4xl xl:text-6xl'>
-                    <span style={{color:'rgb(216, 92, 54)'}} className='font-bold'>Full Stack</span>
-                     &nbsp;Developer
-                    </h1>
-                </div>
-                <div className="h-[1px] lg:h-1 mt-4 bg-[#D85C36] w-1/2 lg:w-4/6" ></div>
-                <div className="text-gray-300 mt-3 lg:mt-4"><p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Inventore laboriosam dignissimos eos rem labore velit sit fuga fugit quam ut. Dolores animi consequatur tempore alias unde laboriosam in consectetur ex modi voluptate, ab veritatis quos ullam suscipit possimus necessitatibus dolore!</p></div>
-                <div className="">
-                    <div className="btn-wrapper text-gray-300 flex space-x-4 my-9">
-                        <div className="btn-1 py-2  border border-[#D85C36] w-28  md:w-40 text-center cursor-pointer  rounded-lg">
-                            <Link to="/about">
-                            <button className=''>
-                                <span className="animate-border"></span>
-                                <span>More About Me</span>
-                            </button>
-                            </Link>
-                        </div>
-                        <div className="btn-2 py-2 border border-[#D85C36] w-28 md:w-40 text-center rounded-lg cursor-pointer">
-                            <Link to="/contact">
-                            <button className='relative'>
-                                <span className="animate-border-2"></span>
-                                <span>Contact Us</span>
-                            </button>
-                            </Link>
-                        </div>
-                    </div>
-                </div>
+          <div className="left flex-1 text-xs sm:text-sm md:text-base xl:flex-[1.3]">
+            <div className="leading-8">
+              <h1 className="text-xl md:text-2xl lg:text-4xl xl:text-6xl">
+                Hey, I am a
+              </h1>
             </div>
-            <div className="right flex flex-1 items-center justify-center">
-                <div className="blurb-wrapper">
-                <div className="blurb-logo h-1/2 w-1/2 bg-slate-200 ">
-                </div>
-                blur
-                </div>
+            <div className="">
+              <h1 className="text-xl md:text-2xl lg:text-4xl xl:text-6xl">
+                <span
+                  style={{ color: "rgb(216, 92, 54)" }}
+                  className="font-bold"
+                >
+                  Full Stack
+                </span>
+                &nbsp;Developer
+              </h1>
             </div>
+            <div className="h-[1px] lg:h-1 mt-4 bg-[#D85C36] w-1/2 lg:w-4/6"></div>
+            <div className="text-gray-300 mt-3 lg:mt-4">
+              <p>
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                Inventore laboriosam dignissimos eos rem labore velit sit fuga
+                fugit quam ut. Dolores animi consequatur tempore alias unde
+                laboriosam in consectetur ex modi voluptate, ab veritatis quos
+                ullam suscipit possimus necessitatibus dolore!
+              </p>
+            </div>
+            <div className="">
+              <div className="btn-wrapper text-gray-300 flex space-x-4 my-9">
+                <Link to="/about">
+                  <div className="btn-1 py-2  border border-[#D85C36] w-28  md:w-40 text-center cursor-pointer  rounded-lg">
+                    <button className="">
+                      <span className="animate-border"></span>
+                      <span>More About Me</span>
+                    </button>
+                  </div>
+                </Link>
+                <Link to="/contact">
+                  <div className="btn-2 py-2 border border-[#D85C36] w-28 md:w-40 text-center rounded-lg cursor-pointer">
+                    <button className="relative">
+                      <span className="animate-border-2"></span>
+                      <span>Contact Us</span>
+                    </button>
+                  </div>
+                </Link>
+              </div>
+            </div>
+          </div>
+          <div className="right flex flex-1 items-center justify-center">
+            <div className="blurb-wrapper">
+              <div className="blurb-logo h-1/2 w-1/2 bg-slate-200 "></div>
+              blur
+            </div>
+          </div>
         </div>
-    </motion.section>
+      </motion.section>
     </>
-  )
+  );
 }
 
-export default Home
+export default Home;
