6e7ca7152c1b0696e7bd904b72f13a1e8c3bebd0
Aman <webdevaman2@gmail.com>
2023-02-13 11:25:51 +0530

    added lazy loading

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { Link, Route, Routes } from 'react-router-dom'
-import React from 'react';
+import React, { Suspense } from 'react';
 import './App.css'
 import CustomPointer from './components/CustomPointer'
 // const Home = React.lazy(()=>import('./components/Home'));
@@ -10,19 +10,32 @@ import About from './components/AboutMe'
 import Contact from './components/Contact'
 import Hero from './components/Hero'
 import Home from './components/Home' 
-import LeftBar from './components/leftBar';
-import RightBar from './components/RightBar';
-import MobileNav from './components/MobileNav';
 function App() {
   return (
     <>
   <CustomPointer/>
     {/* <Hero/> */}
     <Routes>
-        <Route exact path="/" element={<Hero/>}>
-          <Route index element={<Home/>} />
-          <Route path="contact" element={<Contact/>}/>
-          <Route path="about" element={<About/>}/>
+        <Route exact path="/" element={
+          <Suspense fallback={<div>Loading...</div>}>
+          <Hero/>
+          </Suspense>
+        }>
+          <Route index element={
+             <Suspense fallback={<div>Loading...</div>}>
+                <Home/>
+             </Suspense>
+          } />
+          <Route path="contact" element={
+             <Suspense fallback={<div>Loading...</div>}>
+          <Contact/>
+             </Suspense>
+          }/>
+          <Route path="about" element={
+            <Suspense fallback={<div>Loading...</div>}>
+          <About/>
+          </Suspense>
+          }/>
         </Route>
     </Routes> 
     </>
