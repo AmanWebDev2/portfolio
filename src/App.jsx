7fc9dccde3d012876970585b6ecf1b646b388a6a
Aman <webdevaman2@gmail.com>
2023-02-13 11:43:38 +0530

    added lazy loading to others component also

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,14 @@ import { Link, Route, Routes } from 'react-router-dom'
 import React, { Suspense } from 'react';
 import './App.css'
 import CustomPointer from './components/CustomPointer'
-// const Home = React.lazy(()=>import('./components/Home'));
-// const About = React.lazy(()=>import('./components/AboutMe'));
-// const Hero = React.lazy(()=>import('./components/Hero'));
-// const Contact = React.lazy(()=>import('./components/Contact'));
-import About from './components/AboutMe'
-import Contact from './components/Contact'
-import Hero from './components/Hero'
-import Home from './components/Home' 
+const Home = React.lazy(()=>import('./components/Home'));
+const About = React.lazy(()=>import('./components/AboutMe'));
+const Hero = React.lazy(()=>import('./components/Hero'));
+const Contact = React.lazy(()=>import('./components/Contact'));
+// import About from './components/AboutMe'
+// import Contact from './components/Contact'
+// import Hero from './components/Hero'
+// import Home from './components/Home' 
 function App() {
   return (
     <>
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,18 +1,29 @@
-import React, { useEffect, useRef, useState } from "react";
-import Home from "./Home";
-import MobileNav from "./MobileNav";
-import LeftBar from "./leftBar";
-import RightBar from "./RightBar";
+import React, { Suspense } from "react";
+// import MobileNav from "./MobileNav";
+// import LeftBar from "./leftBar";
+// import RightBar from "./RightBar";
 import { Outlet } from "react-router-dom";
+
+const MobileNav = React.lazy(()=>import('./MobileNav'));
+const LeftBar = React.lazy(()=>import('./LeftBar'));
+const RightBar = React.lazy(()=>import('./RightBar'));
  
 function Hero() {
   return (
     <>
         <div className="flex items-center justify-items-center min-w-full min-h-screen">
+        <Suspense fallback={<div>Loading...</div>}>
           <LeftBar />
+        </Suspense>
           <Outlet/>
+
+          <Suspense fallback={<div>Loading...</div>}>
           <RightBar />
+          </Suspense>
+
+          <Suspense fallback={<div>Loading...</div>}>
           <MobileNav />
+          </Suspense>
         </div>
     </>
   );
