75c02257cbc8888a2b98c93ce2fcb859292ce06b
Aman <webdevaman2@gmail.com>
2023-03-07 21:35:38 +0530

    fixed about headline

--- src/components/PageHeadline.jsx
+++ src/components/PageHeadline.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const PageHeadline = ({ headline }) => {
   return (
-    <div className="title text-center">
+    <div className="title text-center w-max	mx-auto">
       <div className="contact-title text-xl md:text-2xl text-gray-200 uppercase tracking-widest font-semibold">
         {headline}
       </div>
