c79ef215e4d6b23bc0072eee8b8bc61e360fd815
Aman <webdevaman2@gmail.com>
2023-03-22 21:36:31 +0530

    added not found page

--- src/components/NotFound.jsx
+++ src/components/NotFound.jsx
@@ -1,13 +1,27 @@
-import React from 'react'
-import NotFoundImg from "../assets/not-found.jpg"
+import React from "react";
+import { Link } from "react-router-dom";
+import NotFoundImg from "../assets/not-found2.jpg";
+import RocketSvg from "../assets/SVG/RocketSvg";
 const NotFound = () => {
   return (
-      <section className="common-seciton lg:w-[80%] text-xs sm:text-sm md:text-base">
-        <div>
-         <img className='w-8/12 mx-auto rounded-2xl' src={NotFoundImg} alt="" />
-        </div>
-  </section>
-  )
-}
+    <section className="common-seciton lg:w-[80%] text-xs sm:text-sm md:text-base notfound_pg">
+      <div className="not_found_svg_container">
+       <RocketSvg/>
+      </div>
+      <div class="text">
+        <h1 className="text-2xl">404 Error</h1>
+        <h2>Couldn't launch :(</h2>
+        <h3>
+          Page Not Found - lets take you&nbsp;
+          <Link to="/"> 
+          <span className="font-bold">
+            BACK
+          </span>
+           </Link>
+        </h3>
+      </div>
+    </section>
+  );
+};
 
-export default NotFound
+export default NotFound;
